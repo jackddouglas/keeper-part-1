18688ab93d2ad990e825e2b1405fa407c5fc0cb6
Jack D. Douglas <jackdouglas2@me.com>
2020-09-14 21:46:41 -0700

    add css to base code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const year = new Date().getFullYear()
 
 function Footer () {
-  return <p>Copyright &copy; {year}</p>
+  return (
+    <footer>
+      <p>Copyright &copy; {year}</p>
+    </footer>
+  )
 }
 
 export default Footer
